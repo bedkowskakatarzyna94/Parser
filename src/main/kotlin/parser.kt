79ebb2c508f96d3d27086e5d98d32a958bac1fad
Katarzyna Bedkowska <kbedkowska@C12339>
2022-07-28 12:27:53 +0200

    Parenthesis handling

--- src/main/kotlin/parser.kt
+++ src/main/kotlin/parser.kt
@@ -1,4 +1,3 @@
-
 fun calculateExpression(expression: MutableList<String>): Int {
 
     val operators = listOf("*", "/")
@@ -38,9 +37,41 @@ fun calculateExpression(expression: MutableList<String>): Int {
     return result
 }
 
+fun findClosingParenthesisIndex(openingBraceIndex: Int, expression: MutableList<String>): Int {
+    if (openingBraceIndex != -1) {
+        for (index in openingBraceIndex until expression.size)
+            if (expression[index] == ")") {
+                return index
+            }
+    }
+    return -1
+}
+
+
+//fun validateExpression(expression: MutableList<String>): Boolean{
+//
+//}
+
 
 fun main() {
+
     print("Enter a mathematical expression (separate each number and operator with a space): ")
     val expression: MutableList<String> = readLine()!!.split(' ').toMutableList()
-    println(calculateExpression(expression))
+
+    while (true) {
+        val openingParenthesisIndex = expression.indexOfLast { it == "(" }
+        val closingParenthesisIndex = findClosingParenthesisIndex(openingParenthesisIndex, expression)
+
+        if ((openingParenthesisIndex != -1) and (closingParenthesisIndex != -1)) {
+            val partialExpression = expression.slice((openingParenthesisIndex + 1) until closingParenthesisIndex)
+            val resultOfPartialExpression = calculateExpression(partialExpression as MutableList<String>)
+            expression.subList(openingParenthesisIndex, closingParenthesisIndex).clear()
+            expression[openingParenthesisIndex] = resultOfPartialExpression.toString()
+        } else {
+            break
+        }
+    }
+    val result = calculateExpression(expression)
+    println("Result: $result")
+
 }
